5b4561f06e1f3b20c80aa64cb3e49dc6811df83b
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-07-29 10:39:59 +0200

    ring4: refactor: simplify ServerApp constructor and remove MainWindow dependency

--- server/ServerApp.cpp
+++ server/ServerApp.cpp
@@ -1,45 +1,20 @@
 #include "ServerApp.hpp"
-#include "MainWindow.hpp"
 
-#include <QMetaObject>
-#include <QString>
-#include <QObject>
-#include <QDebug>
-
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <cstring>
-
-#ifdef _WIN32
-  #include <winsock2.h>   // for select(), FD_* on Windows
-  #include <ws2tcpip.h>
-  #pragma comment(lib, "ws2_32.lib")
-#else
-  #include <sys/select.h>
-#endif
-
-ServerApp::ServerApp(const std::string& envFilePath, MainWindow* ui)
-  : m_envPath(envFilePath)
-  , m_ui(ui)
+ServerApp::ServerApp(const std::string& envFilePath)
+  : m_envFilePath(envFilePath)
 {
-    // Console en UTF-8
-#ifdef _WIN32
+    // UTF-8 console on Windows
+  #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-#endif
+  #endif
 
-    // Charger .env et récupérer le port
-    EnvLoader::loadEnv(m_envPath);
-    int port = EnvLoader::loadPort(m_envPath);
-    std::cout << "ServerApp: binding on port " << port << "...\n";
+    // Load .env
+    EnvLoader::loadEnv(m_envFilePath);
+    int port = EnvLoader::loadPort(m_envFilePath);
+    std::cout << "ServerApp: écoute sur le port " << port << "...\n";
 
-    // Initialiser Winsock et le socket serveur
-#ifdef _WIN32
-    LPTF_Socket::initialize();
-#else
-    LPTF_Socket::initialize();
-#endif
+    // Bind & listen
     m_serverSocket.bindSocket(port);
     m_serverSocket.listenSocket();
 }
@@ -48,102 +23,64 @@ ServerApp::~ServerApp() {
     m_serverSocket.closeSocket();
 }
 
-int ServerApp::run() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-    try {
-        qDebug() << "Serveur prêt. En attente de connexions…\n";
+void ServerApp::run() {
+    std::cout << "Serveur prêt. En attente de connexions...\n";
 
-        while (true) {
-            // Préparation des fd_set
-            fd_set readfds;
-            FD_ZERO(&readfds);
+    while (true) {
+        // Prepare fd_set
+        fd_set readfds;
+        FD_ZERO(&readfds);
 
-            int listenFd = m_serverSocket.getSocketFd();
-            FD_SET(listenFd, &readfds);
-            int maxFd = listenFd;
+        int listenFd = m_serverSocket.getSocketFd();
+        FD_SET(listenFd, &readfds);
+        int maxFd = listenFd;
 
-            for (const auto& client : m_clients) {
-                int fd = client->getSocketFd();
-                FD_SET(fd, &readfds);
-                if (fd > maxFd) maxFd = fd;
-            }
+        for (const auto& c : m_clients) {
+            int fd = c->getSocketFd();
+            FD_SET(fd, &readfds);
+            if (fd > maxFd) maxFd = fd;
+        }
 
-            int activity = select(maxFd + 1, &readfds, nullptr, nullptr, nullptr);
-            if (activity < 0) {
-                std::cerr << "Erreur lors de select(): " << std::strerror(errno) << "\n";
-                break;
-            }
+        // Blocking until activity
+        int activity = select(maxFd + 1, &readfds, nullptr, nullptr, nullptr);
+        if (activity < 0) {
+            std::cerr << "select() error: " << std::strerror(errno) << "\n";
+            break;
+        }
 
-            // Nouvelle connexion
-            if (FD_ISSET(listenFd, &readfds)) {
-                qDebug() << "Print new connect";
-                auto newClient = m_serverSocket.acceptSocket();
-                std::string ip = newClient->getClientIP();
-                m_clients.push_back(std::move(newClient));
+        // New connection?
+        if (FD_ISSET(listenFd, &readfds)) {
+            auto newCl = m_serverSocket.acceptSocket();
+            std::string ip = newCl->getClientIP();
+            std::cout << "Client connecté: " << ip << "\n";
+            m_clients.push_back(std::move(newCl));
+        }
 
-                if (m_ui) {
-                    QString qip = QString::fromStdString(ip);
-                    QMetaObject::invokeMethod(
-                        m_ui,
-                        "addClientTab",
-                        Qt::QueuedConnection,
-                        Q_ARG(QString, qip)
-                    );
-                }
-            }
+        // Read from existing clients
+        for (auto it = m_clients.begin(); it != m_clients.end(); ) {
+            int fd = (*it)->getSocketFd();
+            if (FD_ISSET(fd, &readfds)) {
+                try {
+                    auto data = (*it)->recvBinary();
+                    auto pkt  = LPTF_Packet::deserialize(data);
+                    std::string msg(pkt.getPayload().begin(), pkt.getPayload().end());
+                    std::cout << "Reçu de " << (*it)->getClientIP() << " : " << msg << "\n";
 
-            // Lecture et dispatch des messages
-            for (auto it = m_clients.begin(); it != m_clients.end();) {
-                int fd = (*it)->getSocketFd();
-                if (FD_ISSET(fd, &readfds)) {
-                    try {
-                        auto data = (*it)->recvBinary();
-                        auto packet = LPTF_Packet::deserialize(data);
-                        std::string payload(packet.getPayload().begin(), packet.getPayload().end());
+                    // Echo back
+                    std::string resp = "ACK: " + msg;
+                    std::vector<uint8_t> pay(resp.begin(), resp.end());
+                    LPTF_Packet ack(1, PacketType::RESPONSE, 0, 0, 0, pay);
+                    (*it)->sendBinary(ack.serialize());
 
-                        if (m_ui) {
-                            QString qip = QString::fromStdString((*it)->getClientIP());
-                            QString qtxt = QString::fromStdString(payload);
-                            QMetaObject::invokeMethod(
-                                m_ui,
-                                "appendClientOutput",
-                                Qt::QueuedConnection,
-                                Q_ARG(QString, qip),
-                                Q_ARG(QString, qtxt)
-                            );
-                        }
-
-                        // Echo/Ack
-                        std::string response = "Reçu : " + payload;
-                        std::vector<uint8_t> respPayload(response.begin(), response.end());
-
-                        // on construit le paquet avec TOUS les paramètres du constructeur :
-                        LPTF_Packet respPkt(
-                            1,                      // version
-                            PacketType::RESPONSE,   // type
-                            0,                      // flags
-                            0,                      // packetId
-                            0,                      // sessionId
-                            respPayload            // payload
-                        );
-                        (*it)->sendBinary(respPkt.serialize());
-
-                        ++it;
-                    } catch (const std::exception& e) {
-                        std::string ip = (*it)->getClientIP();
-                        std::cout << "Client déconnecté (" << ip << "): " << e.what() << "\n";
-                        it = m_clients.erase(it);
-                    }
-                } else {
                     ++it;
+                } catch (const std::exception& e) {
+                    std::cout << "Déconnexion de " << (*it)->getClientIP()
+                              << " (" << e.what() << ")\n";
+                    it = m_clients.erase(it);
                 }
+            } else {
+                ++it;
             }
         }
-    } catch (const std::exception& e) {
-        std::cerr << "Erreur fatale dans ServerApp : " << e.what() << "\n";
-        return 1;
     }
-
-    return 0;
 }
