dd53cefafe6e8e5d00ef72cd3206def1b5841835
baptiste-appriou <baptiste.appriou@laplateforme.io>
2025-07-30 14:39:54 +0200

    ring4: feat: enhance client connection handling and store usernames

--- server/ServerApp.hpp
+++ server/ServerApp.hpp
@@ -36,6 +36,7 @@ signals:
 private:
     LPTF_Socket m_serverSocket;
     std::vector<std::unique_ptr<LPTF_Socket>> m_clients;
+    std::vector<std::string> m_clientUsers;
     std::string m_envFilePath;
 };
 
