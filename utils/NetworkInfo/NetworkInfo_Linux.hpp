008a9dd606641bd78800e3523261afbb5e52d4d8
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-08-04 13:22:54 +0200

    chore(NetworkInfo): enhance documentation for INetworkInfo interface and platform-specific implementations

--- utils/NetworkInfo/NetworkInfo_Linux.hpp
+++ utils/NetworkInfo/NetworkInfo_Linux.hpp
@@ -13,11 +13,35 @@
 #include <cstring>
 #include <sstream>
 
+
+/**
+ * @class NetworkInfo_Linux
+ * @brief Provides network information for Linux systems.
+ */
 class NetworkInfo_Linux : public INetworkInfo {
 public:
+    /**
+     * @brief Gets all local IP addresses.
+     * @return Vector of IP address strings.
+     */
     std::vector<std::string> getIPAddresses() const override;
+
+    /**
+     * @brief Gets all MAC addresses of network interfaces.
+     * @return Vector of MAC address strings.
+     */
     std::vector<std::string> getMACAddresses() const override;
+
+    /**
+     * @brief Gets active (up) IP addresses.
+     * @return Vector of active IP address strings.
+     */
     std::vector<std::string> getActiveIPAddresses() const override;
+
+    /**
+     * @brief Gets active (up) MAC addresses.
+     * @return Vector of active MAC address strings.
+     */
     std::vector<std::string> getActiveMACAddresses() const override;
 };
 
--- utils/NetworkInfo/NetworkInfo_Windows.hpp
+++ utils/NetworkInfo/NetworkInfo_Windows.hpp
@@ -9,12 +9,36 @@
 #include <ws2tcpip.h>
 #include <sstream>
 
+
+/**
+ * @class NetworkInfo_Windows
+ * @brief Provides network information for Windows systems.
+ */
 class NetworkInfo_Windows : public INetworkInfo {
 public:
+    /**
+     * @brief Gets all local IP addresses.
+     * @return Vector of IP address strings.
+     */
     std::vector<std::string> getIPAddresses() const override;
+
+    /**
+     * @brief Gets all MAC addresses of network interfaces.
+     * @return Vector of MAC address strings.
+     */
     std::vector<std::string> getMACAddresses() const override;
+
+    /**
+     * @brief Gets active (up) IP addresses.
+     * @return Vector of active IP address strings.
+     */
     std::vector<std::string> getActiveIPAddresses() const override;
-    std::vector<std::string> getActiveMACAddresses() const override ;
+
+    /**
+     * @brief Gets active (up) MAC addresses.
+     * @return Vector of active MAC address strings.
+     */
+    std::vector<std::string> getActiveMACAddresses() const override;
 };
 
 #endif
